ee129f6e92d63b129a691e89255f48f46326a08a
pjmaker <philip.maker@gmail.com>
2016-01-23 07:09:52 +0930

    Use tsoptions.py
    Signed-off-by: pjmaker <philip.maker@gmail.com>

--- tsi.py
+++ tsi.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python3
-# 
+#
 # tsi.py - time series irregular library
-# 
+#
 # Copyright (c) 2016, Phil Maker
-# 
+#
 # All rights reserved.
-#    
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions 
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
 # are met:
-# 
+#
 #  * Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
 #  * Redistributions in binary form must reproduce the above copyright
@@ -19,7 +19,7 @@
 #  * Neither the name of the copyright-owner nor the names of its
 #    contributors may be used to endorse or promote products derived
 #    from this software without specific prior written permission.
-#        
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -31,9 +31,9 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 
-'''tsi 
+'''tsi
 
 This module....
 
@@ -45,36 +45,14 @@ TODO:
    #. Break this apart into individual components
 '''
 
-# argv handling
-options = {}
-'''dictionary of command line options'''
 
-import sys
-
-# default options
-options = {
-    '-trace':False,
-    '-profile_main':False,
-    '-show_options':False,
-    '-test':1
-}
-
-if not sys.argv[0].endswith('sphinx-build'):
-    if len(sys.argv[1:]) > 0:
-        options.update(sys.argv[1:])
-
-
-# show the options if required
-if options['-show_options']:
-    for o in sorted(options):
-        print('options ' + o.ljust(24) + ' ' + str(options[o]))
 
 import datetime
 import time
 import calendar
 import iso8601
 import math
-import statistics 
+import statistics
 import glob
 import os
 import sys
@@ -84,6 +62,15 @@ import scipy as sp
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+import tsioptions as opts
+
+opts.set({
+    '-trace':False,
+    '-profile_main':False,
+    '-show_options':True,
+    '-test':1
+})
 
 # option setup
 plt.style.use('ggplot')
@@ -133,7 +120,7 @@ def tparse(t):
 
 def tformat(s):
     '''convert a float of seconds since epoch to an ISO8601 date
-    
+
     There is still a bit of representation error in this
     component. Why we aren't using numpy datetime64.
 
@@ -153,7 +140,7 @@ def tformat(s):
       2001-09-09T01:46:40.988000+00:00
 
       Note that we get representation errors in the following
-      two tests. 
+      two tests.
 
       !>>> import tsi
       !>>> print(tsi.tformat(1000000000.989))
@@ -163,11 +150,12 @@ def tformat(s):
       !>>> print(tsi.tformat(tsi.tparse('2001-09-09T01:46:40.989000+00:00')))
       2001-09-09T01:46:40.989000+00:00
     '''
-    return datetime.datetime.fromtimestamp(round(s,3),pytz.UTC).isoformat('T')
+    return datetime.datetime.fromtimestamp(round(s, 3),
+                                           pytz.UTC).isoformat('T')
 
 def tdsecs(td):
     '''convert a timedelta to a number in seconds
-    
+
     Args:
       ts (timedelta): a timedelta from pandas/numpy
 
@@ -178,7 +166,7 @@ def tdsecs(td):
        None
 
     '''
-    
+
     return (td / np.timedelta64(1, 's'))
 
 # support for nan
@@ -189,7 +177,7 @@ nan = float('nan')
 # support for reading data in
 
 hists = {}
-'''{var->[(when,what)]}: history of var as a list of (when,what) events
+'''{var->[(when, what)]}: history of var as a list of (when, what) events
 
 This is used as the basic representation for history of variables
 before we mangle it into the various pandas DataFrame representations.
@@ -197,15 +185,15 @@ before we mangle it into the various pandas DataFrame representations.
 
 def tsvars():
     '''returns variables we have history for
-    
+
     Returns:
     [str]: sorted list of variable names
     '''
     return sorted(hists)
 
 def tsread(fn):
-    '''read file fn and convert [(when,what)...]
-    
+    '''read file fn and convert [(when, what)...]
+
     Args:
       fn (str): filename to read which is in ASIM format
 
@@ -223,7 +211,7 @@ def tsread(fn):
         else:
             t = tparse(t)
             v = float(v)
-            r.append((t,v))
+            r.append((t, v))
         n += 1
     return r
 
@@ -231,7 +219,7 @@ def tsreadfiles(pat, rename):
     '''Read all files matching glob pat and rename var using rename
 
     Args:
-    
+
     pat (str): glob pattern for matching files
     rename (hook): function renaming variables from file to varname
 
@@ -244,27 +232,30 @@ def tsreadfiles(pat, rename):
         hists[var] = tsread(fn)
 
 def tsevents():
-    '''converts hists[] to [(when,var,what)...]
-    
-    Returns: 
-    [(when,var,what)]: similar to an alarm log
+    '''converts hists[] to [(when, var, what)...]
+
+    Returns:
+    [(when, var, what)]: similar to an alarm log
     '''
     global hists
     events = []
     for var in sorted(hists):
-        for (when,what) in hists[var]:
-            events.append((when,var,what))
+        for (when, what) in hists[var]:
+            events.append((when, var, what))
     events.sort()
     # print('tsevents = ', events)
     return events
 
 def tsstates():
     '''converts hists to [when, {var->what}] by remembering state
-    
-    For example [a:[(100,1),(200,10)],b:[(10,-1),(250,-11)]]
-    converts to [(10,{a:nan,b:-1}),(100,{a:1,b:-1}),(200,{a:10,b:-1}),
-    (250,{a:10,b:-11})]
-                 
+
+    For example [a:[(100, 1), (200, 10)], b:[(10, -1), (250, -11)]]
+    converts to
+    [(10, {a:nan, b:-1}),
+    (100, {a:1, b:-1}),
+    (200, {a:10, b:-1}),
+    (250, {a:10, b:-11})]
+
     Examples:
 
     None yet till I redo it
@@ -278,10 +269,10 @@ def tsstates():
     for v in hists:
         state[v] = nan
     lastwhen = nan
-    for (when,var,what) in tsevents():
+    for (when, var, what) in tsevents():
         state[var] = what
         if when != lastwhen:
-            states.append((when,state.copy()))
+            states.append((when, state.copy()))
     return states
 
 def ts2csv(fd):
@@ -302,11 +293,11 @@ def ts2csvbody(fd):
         print(tformat(when), end=',', file=fd)
         for v in tsvars():
             print(state[v], end=',', file=fd)
-        print ('', file=fd)
+        print('', file=fd)
 
 def limit(v, low, high):
     '''limit v between low and high'''
-    return max(min(v,high),low)
+    return max(min(v, high), low)
 
 def scale(v, p=1):
     '''scale v by p'''
@@ -318,10 +309,10 @@ def offset(v, o=0):
 
 def fntovar(fn):
     '''Convert filename to variable name'''
-    fn = fn.replace('data/','')
-    fn = fn.replace('R_K_PG_BULM_','')
-    fn = fn.replace('_1JAN2000_now','')
-    fn = fn.replace('.csv','')
+    fn = fn.replace('data/', '')
+    fn = fn.replace('R_K_PG_BULM_', '')
+    fn = fn.replace('_1JAN2000_now', '')
+    fn = fn.replace('.csv', '')
     return fn
 
 def tsmean(df, v):
@@ -330,25 +321,28 @@ def tsmean(df, v):
 
 def tssummary(df, v):
     '''Return a summary of the variable v in dataframe df.'''
-    s = (v + ' min..tsmean..max = ').ljust(32)  
-    s += str(df[v].min()) + '..' 
-    s += str(round(tsmean(df,v),3)) + '..' 
+    s = (v + ' min..tsmean..max = ').ljust(32)
+    s += str(df[v].min()) + '..'
+    s += str(round(tsmean(df, v), 3)) + '..'
     s += str(df[v].max())
     return s
 
 def getdf(pats):
     '''Return DataFrame from files matching members of pats.
-    
+
     pats - list of glob style pattern matching the files to process.
     '''
     for pat in pats:
         tsreadfiles(pat, fntovar)
     ts2csv(open('tmpdata.csv', 'w'))
+    # pylint does not seem to understand what a dataframe is
+    # whence we need to disable the check
+    # pylint:disable=maybe-no-member
     return pd.read_csv('tmpdata.csv', parse_dates=['t']).set_index('t')
 
 def makedt(df):
     '''Return a dataframe with new dt, w* and t* Series.
-    
+
     The new dataframe contains:
 
     df['dt'] - the difference in time between this samples.
@@ -363,21 +357,21 @@ def makedt(df):
     f = lambda x: 0 if isnan(x[0]) else x[1]
     for v in tsvars():
         df['w' + v] = df[v] * df['dt']
-        df['t' + v] = df[[v, 'dt']].apply(f,axis=1)
+        df['t' + v] = df[[v, 'dt']].apply(f, axis=1)
     return df
 
 # wrappers for plot
 
 from matplotlib.backends.backend_pdf import PdfPages
 
-def plotPdf(fn,**kwopts):
+def plotPdf(fn, **kwopts):
     print('plotPdf ' + fn)
     pp = PdfPages(fn)
     year = 2015
-    months = range(10,13)
+    months = range(10, 13)
     for month in months:
-        dmax = calendar.monthrange(year,month)[1]
-        for day in range(1,dmax-1):
+        dmax = calendar.monthrange(year, month)[1]
+        for day in range(1, dmax-1):
             title = str(year) + '-' + str(month) + '-' + str(day)
             print(' plotPdf ' +  title)
             global df
@@ -386,32 +380,32 @@ def plotPdf(fn,**kwopts):
                            # color='black',
                            drawstyle='steps',
                            title=title,
-                           xlim=(pd.datetime(year,month,day,19), 
-                                 pd.datetime(year,month,day+1,10)))
+                           xlim=(pd.datetime(year, month, day, 19),
+                                 pd.datetime(year, month, day+1, 10)))
             pp.savefig()
     pp.close()
     os.system('evince ' + fn)
 
-# trace code 
-# 
+# trace code
+#
 # TODO: update to a better tracer sometime
 #
 
-if options['-trace']:
+if opts.get('-trace'):
     import sys
 
     def tracer(frame, event, arg, indent=[0]):
         # print('tracer', event)
         func = frame.f_code.co_name
-        file = frame.f_code.co_filename 
+        file = frame.f_code.co_filename
         if func[0] == '_' or file[0] == '/':
             return
         if event == "call":
             indent[0] += 2
-            print("-" * indent[0] + "> call function", 
-                      func + ':' + file)
+            print("-" * indent[0] + "> call function",
+                  func + ':' + file)
         elif event == "return":
-            print("<" + "-" * indent[0], "exit function", 
+            print("<" + "-" * indent[0], "exit function",
                   func + ':' + file)
         indent[0] -= 2
         return tracer
@@ -421,7 +415,7 @@ if options['-trace']:
 # profile support
 import cProfile
 import pstats
-    
+
 def profile(c):
     '''profile code c
     Args:
@@ -434,7 +428,7 @@ def profile(c):
 # some 1 liners for testing
 def showeval(s):
     '''print a string followed by its evaluation
-    
+
     Note that python does not have an uplevel(3tcl) command
     like TCL and so need to use globals or the locals=dict
     argument. There must be a better way.
@@ -448,7 +442,7 @@ def main():
 def test1():
     '''run a simple test
 
-    Note 
+    Note
     '''
     print('* test1() - basic input and statistics for 1 series')
     global df # we need global dataframe so showeval can see it.
@@ -469,7 +463,7 @@ def test1():
 def test1_2():
     '''run a simple test
 
-    Note 
+    Note
     '''
     print('* test1_2() - basic input and statistics for 2 series')
     global df # we need global dataframe so showeval can see it
@@ -478,7 +472,7 @@ def test1_2():
     print(open('data/Test1.csv').read())
     print(open('data/Test2.csv').read())
     print("df = makedt(getdf(['data/Test1.csv','data/Test2.csv']))")
-    df = makedt(getdf(['data/Test1.csv','data/Test2.csv']))
+    df = makedt(getdf(['data/Test1.csv', 'data/Test2.csv']))
     showeval("print(df)")
     showeval("print(df.describe())")
     print('** statistics - N.B. weighting seems odd')
@@ -494,47 +488,48 @@ def test1_2():
     showeval("tsmean(df,'Test2')")
 
 def rest2():
-    df = makedt(getdf(['data/*SkyCam1*',
-                       'data/*Fed3Pact*',
-                   ]))
-    
+    df = makedt(getdf(['data/*SkyCam1*','data/*Fed3Pact*']))
+
 def rest():
     '''Just a block to keep scrap code in'''
     # tidy up the data
     if False:
-        df['SkyCam1_2mOk'] = df['SkyCam1_2mOk'].apply(lambda x: offset(limit(x,0,0.5),-1))
-        df['SkyCam2_2mOk'] = df['SkyCam2_2mOk'].apply(lambda x: offset(limit(x,0,0.5),-2))
-        df['SkyCam3_2mOk'] = df['SkyCam3_2mOk'].apply(lambda x: offset(limit(x,0,0.5),-3))
-        
-        df['Fed3Pact'] = df['Fed3Pact'].apply(lambda x: scale(x,0.1))
-        
+        df['SkyCam1_2mOk'] = df['SkyCam1_2mOk'].apply(
+            lambda x: offset(limit(x, 0, 0.5), -1))
+        df['SkyCam2_2mOk'] = df['SkyCam2_2mOk'].apply(
+            lambda x: offset(limit(x, 0, 0.5), -2))
+        df['SkyCam3_2mOk'] = df['SkyCam3_2mOk'].apply(
+            lambda x: offset(limit(x, 0, 0.5), -3))
+
+        df['Fed3Pact'] = df['Fed3Pact'].apply(lambda x: scale(x, 0.1))
+
         # # df['SkyCam2_2mOk'] = df['SkyCam2_2mOk'].apply(lambda x: limit(x,0,1))
         # # df['SkyCam3_2mOk'] = df['SkyCam3_2mOk'].apply(lambda x: limit(x,0,1))
-        
-        
+
+
         # do some basic statistics
         if False:
             for v in tsvars():
-                print(tssummary(df,v))
-                
+                print(tssummary(df, v))
+
                 if False:
                     tb = 0
-                    for (t,v) in hists['SkyCam2_2mOk']:
-                        print((t - tb),v) 
-                        tb = t 
-                        
+                    for (t, v) in hists['SkyCam2_2mOk']:
+                        print((t - tb), v)
+                        tb = t
+
                         # do some plots
                         # plt.ion() -- interactive
-                        
+
                         if False:
                             for v in tsvars():
                                 df[v].plot(kind='line',
                                            drawstyle='steps',
                                            title='title',
-                                           xlim=(pd.datetime(2015,10,18,19), 
-                                                 pd.datetime(2015,12,25,10)))
+                                           xlim=(pd.datetime(2015, 10, 18, 19),
+                                                 pd.datetime(2015, 12, 25, 10)))
                                 plt.show()
-                                
+
                                 if False:
                                     plotPdf('daily.pdf')
 
@@ -544,9 +539,7 @@ def rest():
 
 # finally call main (or profile it)
 if __name__ == '__main__':
-    if options['-profile_main']:
+    if opts.get('-profile_main'):
         profile('main()')
     else:
         main()
-
-
